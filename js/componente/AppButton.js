d2f8d0e3c2a7f0c8a92c0ddd00dafa6f6c295ce2
mozahid679 <mozahid679@gmail.com>
2023-06-01 11:41:10 +0600

    , Component Props. Added a spinner in button. a fully functional button conponent built

--- js/componente/AppButton.js
+++ js/componente/AppButton.js
@@ -1,9 +1,27 @@
 export default {
-   template: `<button class="bg-gray-200 hover:bg-gray-300 border rounded px-4 py-2 disabled:cursor-not-allowed" :disabled="processing"> <slot/></button>`,
+   template: `
+      <button
+          :class="{
+            ' border  rounded px-5 py-2    disabled:cursor-not-allowed' : true,
+            'bg-blue-500 hover:bg-blue-600': type==='primary',
+            'bg-yellow-200 hover:bg-yellow-300': type==='secondary',
+            'bg-gray-200 hover:bg-gray-300': type==='muted',
+            'is-loading': processing,
+         }" 
+          :disabled="processing"
+          >
+            <slot/>
+      </button>
+      `,
 
-   data() {
-      return {
-         processing: true,
-      };
+   props: {
+      type: {
+         type: String,
+         default: "primary",
+      },
+      processing: {
+         type: Boolean,
+         default: false,
+      },
    },
 };
